4cdf609b7c0d27f4eecaee34aa068496529efded
Abhishek Poddar <67195536+abhi8054@users.noreply.github.com>
2024-09-08 02:38:56 +0530

    invoice added

--- src/app/api/invoice-download/route.js
+++ src/app/api/invoice-download/route.js
@@ -1,47 +1,87 @@
-import puppeteer from "puppeteer";
 import { NextResponse } from "next/server";
+import { admin } from "@/firebaseAdmin/config";
+import { generateInvoiceHtml } from "../../../utils/generateInvoiceHtml";
+const chromium = require("@sparticuz/chromium");
+
+const db = admin.firestore();
 
 export const POST = async (req) => {
-  const {  html } = await req.json();
-  const url="";
-
-  try {
-    const browser = await puppeteer.launch({
-      executablePath : '/home/sbx_user1051/.cache/puppeteer'
-    });
-    const page = await browser.newPage();
-
-    if (url) {
-      await page.goto(url, { waitUntil: "networkidle2" });
-    } else if (html) {
-      await page.setContent(html, { waitUntil: "networkidle2" });
+  try{
+    const {  userId,orderId } = await req.json();
+  
+    const orderDoc = await db.collection('orders').doc(orderId).get();
+    let order;
+  if (orderDoc.exists && orderDoc.data().userId === userId) {
+    order = {
+      _id : orderDoc.id,
+      ...orderDoc.data()
     }
-
+    const html = generateInvoiceHtml(order)
+    let puppeteer;
+    let browser;
+    if(process.env.NEXT_PUBLIC_ENVIRONMENT === 'Production'){
+      puppeteer = require("puppeteer-core");
+      browser = await puppeteer.launch({
+        args: chromium.args,
+        executablePath: await chromium.executablePath(),
+        headless: 'new',
+        ignoreHTTPSErrors: true,
+      });
+    }else{
+      puppeteer = require("puppeteer");
+      browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox','--font-render-hinting=none'],
+        headless : 'new',
+        ignoreHTTPSErrors: true,
+      })
+    }
+  
+    const page = await browser.newPage();
+    await page.setContent(html, { waitUntil: "networkidle2" });
+    await page.emulateMediaType("screen");
+    await page.setViewport({
+      width: 1200,
+      height: 800,
+    });
+  
     const pdfBuffer = await page.pdf({
       format: "A4",
+      margin: { top: "50px", right: "50px", bottom: "50px", left: "50px" },
       printBackground: true,
+      preferCSSPageSize: true,
     });
-
+  
     await browser.close();
-
-    return new NextResponse(pdfBuffer, {
-      headers: {
-        "Content-Type": "application/pdf",
-        "Content-Disposition": "attachment; filename=generated.pdf",
+    console.log(pdfBuffer);
+    return NextResponse.json(
+      {
+        code: 1,
+        message: "Invoice generated successfully.",
       },
-    });
-  } catch (error) {
-    console.error(error);
-    return new NextResponse(
-      JSON.stringify({
+      {
+        status: 200,
+      }
+    );
+  }else{
+    return NextResponse.json(
+      {
         code: 0,
-        message: "Internal Server Error",
-      }),
+        message: "Order not fount",
+      },
+      {
+        status: 404,
+      }
+    );
+  }
+
+  }catch(err) {
+    return NextResponse.json(
+      {
+        code: 0,
+        message: "Internal server error",
+      },
       {
         status: 500,
-        headers: {
-          "Content-Type": "application/json",
-        },
       }
     );
   }
---
+++ src/utils/generateInvoiceHtml.js
@@ -0,0 +1,168 @@
+import { formatTimestampToCustomDate } from "./commonFunctions";
+
+const calculateTax = (_order) => {
+  const { additional, kulcha } = _order;
+  const additionalTotal = additional?.reduce((acc, value) => {
+    return (acc =
+      acc +
+      Number(value?.items?.[0]?.price) * Number(value?.items?.[0]?.quantity));
+  }, Number(kulcha?.price) * Number(kulcha.quantity));
+  return Number(Number(additionalTotal) * 0.13).toFixed(2);
+};
+
+export const generateInvoiceHtml = (order) => {
+  let rows = ``;
+  for (let item of order.order) {
+    const { order } = item;
+    rows =
+      rows +
+      `<tr>
+            <td>${order.kulcha.name}</td>
+            <td>${order.kulcha.quantity}</td>
+            <td>13%</td>
+            <td>${calculateTax(order)}</td>
+            <td>$${item.total_amount}</td>
+          </tr>`;
+  }
+
+  let html = `<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Invoice</title>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        justify-content: center;
+        align-items: flex-start;
+        height: 100vh;
+        background-color: #f7f7f7;
+      }
+
+      .invoice-container {
+        background-color: white;
+        padding: 20px;
+        margin-top: 10px;
+        border: 1px solid #ddd;
+        width: 900px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      }
+
+      .header {
+        text-align: center;
+        margin-bottom: 20px;
+      }
+
+      .header h2 {
+        margin: 0;
+        font-size: 18px;
+      }
+
+      .invoice-details {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 20px;
+      }
+
+      .invoice-details .left,
+      .invoice-details .right {
+        font-size: 14px;
+        line-height: 1.5;
+      }
+
+      .invoice-table {
+        width: 100%;
+        border-collapse: collapse;
+        margin-bottom: 20px;
+      }
+
+      .invoice-table th,
+      .invoice-table td {
+        border: 1px solid #ddd;
+        padding: 8px;
+        text-align: left;
+        font-size: 14px;
+      }
+
+      .invoice-table th {
+        background-color: #f4f4f4;
+      }
+
+      .total {
+        font-size: 14px;
+        margin-bottom: 20px;
+        text-align: right;
+      }
+
+      .total p {
+        margin: 5px 0;
+      }
+
+      .total strong {
+        font-size: 16px;
+      }
+
+      .footer {
+        font-size: 12px;
+        text-align: center;
+        margin-top: 20px;
+        color: #555;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="invoice-container">
+      <div class="header">
+        <h2>Tax Invoice</h2>
+      </div>
+      <div class="invoice-details">
+        <div class="left">
+          <p>Canada</p>
+          <p>Invoice number: ${order._id}</p>
+       
+          <p>Invoice date: ${formatTimestampToCustomDate(order.createdAt)}</p>
+        </div>
+        <div class="right">
+          <p>Invoice issued by Patty Kulcha Limited.</p>
+          <p>HST number: 75491142RT0001</p>
+          <p>GST number: 75491142RT0001</p>
+          <!-- <p>on behalf of:</p>
+          <p>OLGA SOSNORA</p>
+          <p>Canada</p> -->
+        </div>
+      </div>
+      <table class="invoice-table">
+        <thead>
+          <tr>
+            <th>Order item</th>
+            <th>Quantity</th>
+            <th>Tax</th>
+            <th>Tax Amount</th>
+            <th>Total Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${rows}
+        </tbody>
+      </table>
+      <div class="total">
+        <p>Total net amount: $${Number(order.basic_amount).toFixed(2)}</p>
+        <p>Total HST 13%: $${Number(order.total_tax).toFixed(2)}</p>
+        <p>Total amount payable: $${Number(order.grand_total).toFixed(2)}</p>
+      </div>
+      <div class="footer">
+        <p>Issued on behalf of OLGA SOSNORA by:</p>
+        <p>
+          UBER RASIER CANADA INC. // GST: 764945663RC0001 / QST: 1228538970
+          TQ0001
+        </p>
+      </div>
+    </div>
+  </body>
+</html>`;
+
+  return html;
+};
